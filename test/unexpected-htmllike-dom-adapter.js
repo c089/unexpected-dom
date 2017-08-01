585047f1bd52cfc52d2461439119fef166a5da1f
Christoph Neuroth <christoph.neuroth@gmail.com>
2017-08-01 20:15:44 +0200

    Extract adapter

--- test/unexpected-htmllike-dom-adapter.js
+++ test/unexpected-htmllike-dom-adapter.js
@@ -12,43 +12,7 @@ var UnexpectedHtmlLike = require('unexpected-htmllike');
 var unexpectedDom = require('../lib/index');
 var expect = unexpected.clone().installPlugin(unexpectedDom);
 
-function extractDoctype(node) {
-  var doctype = node.doctype;
-  return {
-    name: doctype.name,
-    publicId: doctype.publicId || '',
-    systemId: doctype.systemId || ''
-  };
-}
-
-var adapter = {
-  getName: function (node) {
-    return node.nodeName;
-  },
-  getChildren: function (node) {
-
-    var nodes = Array.prototype.slice.call(node.childNodes);
-    return nodes.map(node => {
-      if (node.nodeType === 3) {
-        return node.textContent;
-      }
-      return node;
-    });
-  },
-  getAttributes: function (node) {
-    var attributes = {};
-    var isRoot = node.parentNode === null;
-    if (isRoot) {
-      return extractDoctype(node);
-    }
-    var attributeNames = (node.getAttributeNames && node.getAttributeNames() || []);
-
-    attributeNames.forEach(function (attributeName) {
-      attributes[attributeName] = node.getAttribute(attributeName);
-    });
-    return attributes;
-  }
-};
+var adapter = require('../lib/htmllike').adapter;
 
 var createDocument = function (html) {
   return new jsdom.JSDOM(html).window.document;
